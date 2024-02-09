7c96d76f72f0dda434adb72ecfbf491c560ca08a
Cosmo Page Croft <page-croft.c@etoncollege.org.uk>
2024-02-09 09:43:04 +0000

    Added Alert for when an ingredient is added or deleted

--- Yummi App/ContentView.swift
+++ Yummi App/ContentView.swift
@@ -17,13 +17,15 @@ struct ContentView: View {
     @State private var enteredExpiryDate: String = ""
     @State private var isShowingFields = false
     @State private var ingredients = Ingredient.examples
+    @State private var newIngredientAlert = false
+    @State private var deleteIngredientAlert = false
     
     var body: some View {
         Form {
             Section(header: Text("🍎 Yummi 🍎")
                 .font(.largeTitle)
                 .padding(20)){
-            
+                    
                     Text(ingredients[selectedIngredientIndex].displayDetails())
                         .padding(20)
                     
@@ -49,6 +51,10 @@ struct ContentView: View {
                             selectedIngredientIndex = (ingredients).count - 1
                             
                             isShowingFields = false
+                            
+                            
+                            newIngredientAlert.toggle()
+                            
                         })
                         
                         
@@ -58,20 +64,29 @@ struct ContentView: View {
                         
                         ingredients.remove(atOffsets: [selectedIngredientIndex])
                         selectedIngredientIndex = (selectedIngredientIndex + 1) % ingredients.count
+                        
+                        deleteIngredientAlert.toggle()
                     })
                     
-                
+                    
                     
                     
                 }
                 .padding()
+            
+        }
+        .alert("New Ingredient Created", isPresented: $newIngredientAlert) {
+            Button("OK", action: {})}
+        .alert("Ingredient Deleted", isPresented: $deleteIngredientAlert) {
+            Button("OK", action: {})}
+                
+        
+    }
+    
+}
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
         }
     }
-}
     
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
-
